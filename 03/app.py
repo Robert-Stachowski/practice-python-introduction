efb9ed5b750affb68d7a80fb6e8b12871d0a492f
Robert-Stachowski <robertstachowski0@gmail.com>
2025-06-23 17:04:41 +0200

    okreslenie_typow_zmiennych app.py

--- 03/app.py
+++ 03/app.py
@@ -1,1 +1,10 @@
+Liczby_całkowite = 10
+Liczby_zmiennoprzecinkowe = 10.1
+tekst = "tadam!"
+wartosci_logicze = "ale ciezko mi tu zdefiniowac"
+lista = ["cos_tam" , "cos_tam_2" , 22]
+krotka = (10 , 20 , 8)
+zbior = {1, 3, 5, 7)
+slownik = {"stan" : "nowy" , "dlugosc" : 30}
+
 
