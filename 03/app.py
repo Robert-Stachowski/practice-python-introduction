7d123b90eda9b9cc9d3a17610f30d87a47081c5d
Robert Stachowski <RobertStachowski0@gmail.com>
2025-06-24 09:49:09 +0200

    added print function

--- 03/app.py
+++ 03/app.py
@@ -1,10 +1,20 @@
-Liczby_całkowite = 10
-Liczby_zmiennoprzecinkowe = 10.1
+liczby_calkowite = 10
+liczby_zmiennoprzecinkowe = 10.1
 tekst = "tadam!"
 wartosci_logicze = "ale ciezko mi tu zdefiniowac"
 lista = ["cos_tam" , "cos_tam_2" , 22]
 krotka = (10 , 20 , 8)
-zbior = {1, 3, 5, 7)
+zbior = {1, 3, 5, 7}
 slownik = {"stan" : "nowy" , "dlugosc" : 30}
 
+print(liczby_calkowite)
+print(liczby_zmiennoprzecinkowe)
+print(tekst)
+print(*lista)
+print(*krotka)
+print(*zbior)
+print(slownik)
+print()
 
+if wartosci_logicze : True
+print(wartosci_logicze)
