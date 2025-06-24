414b8769004342f998ac98f881ab13dcbb5c6ae8
Robert Stachowski <RobertStachowski0@gmail.com>
2025-06-24 11:08:01 +0200

    zmienne plus f-string

---
+++ 04/app.py
@@ -0,0 +1,12 @@
+zmienna_imie = "Robert"
+zmienna_tygodnie = 1
+zmienna_cel = "be the best ;P "
+
+print(
+    f"Mam na imię {zmienna_imie} od " 
+    f"{zmienna_tygodnie} tygodnia uczę się programowania. "
+    f"Moim celem jest {zmienna_cel}"
+    f"\n"
+    f"Nie było to łatwe zadanie dla laika, " 
+    f"całe szczęście że mamy dobrodziejstwo internetów ;) "
+)
