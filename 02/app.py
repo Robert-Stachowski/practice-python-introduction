c75c9a489eca442c6c05f5a1c84b14e263878a09
Robert Stachowski <RobertStachowski0@gmail.com>
2025-06-24 08:23:54 +0200

    poprawienie bledow skladni if

--- 02/app.py
+++ 02/app.py
@@ -1,10 +1,9 @@
 number = 10
 # Przypisanie wartości zmiennej "number"
-if:
-  number < 8
+if number < 8:
   print("Liczba mniejsza niż 10")
 else:
   print("Liczba większa niż 10")
-# """ Komentarz wielolinijkowy, 
-# ale coś kolorów nie chce zmienić"""
+""" Komentarz wielolinijkowy, 
+ ale coś kolorów nie chce zmienić"""
 # O! Hashtag musi wjechać to zaczyna traktować jako komentarz...
